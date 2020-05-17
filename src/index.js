48ef8c4965b6a465003784421f5813236250a4dd
ParkKeonWoong <45916045+ParkKeonWoong@users.noreply.github.com>
2020-05-17 16:29:04 +0900

    React-hooks-usetTaps

--- src/index.js
+++ src/index.js
@@ -1,24 +1,42 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-const useInput = initialValue => {
-  const [value, setValue] = useState(initialValue);
-  const onChange = event => {
-    const {
-      target: { value }
-    } = event;
-    setValue(value);
+import "./useInput";
+
+const content = [
+  {
+    tab: "Section 1",
+    content: "I'm the Content of the Section 1"
+  },
+  {
+    tab: "Section 2",
+    content: "I'm the Content of the Section 2"
+  },
+  {
+    tab: "Section 3",
+    content: "I'm the Content of the Section 3"
+  }
+];
+
+const useTaps = (initialTab, allTabs) => {
+  if (!allTabs || !Array.isArray(allTabs)) {
+    return;
+  }
+  const [currentIndex, setCurrentIndex] = useState(initialTab);
+  return {
+    currentItem: allTabs[currentIndex],
+    changeItem: setCurrentIndex
   };
-  return { value, onChange };
 };
 
 const App = () => {
-  const name = useInput("Mr. Park");
-
+  const { currentItem, changeItem } = useTaps(1, content);
   return (
     <div className="App">
-      <h1>Hello</h1>
-      <input placeholder="Name" value={name.value} onChange={name.onChange} />
+      {content.map((section, index) => (
+        <button onClick={() => changeItem(index)}>{section.tab}</button>
+      ))}
+      <div>{currentItem.content}</div>
     </div>
   );
 };
