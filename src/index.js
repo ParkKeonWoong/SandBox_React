1f1f630530e17557f5110a0a616ae1d6e1268944
ParkKeonWoong <45916045+ParkKeonWoong@users.noreply.github.com>
2020-05-17 15:49:17 +0900

    React-hooks-useInput

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
+const useInput = initialValue => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = event => {
+    const {
+      target: { value }
+    } = event;
+    setValue(value);
+  };
+  return { value, onChange };
+};
+
 const App = () => {
+  const name = useInput("Mr. Park");
+
   return (
     <div className="App">
       <h1>Hello</h1>
-      <input placeholder="Name" />
+      <input placeholder="Name" value={name.value} onChange={name.onChange} />
     </div>
   );
 };
