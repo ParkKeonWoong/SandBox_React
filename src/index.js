2a5ae6f3e696025b925b4c480510c0774164cb2f
ParkKeonWoong <45916045+ParkKeonWoong@users.noreply.github.com>
2020-05-26 02:02:32 +0900

    React-hooks-useNetwork

--- src/index.js
+++ src/index.js
@@ -1,10 +1,34 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import ReactDOM from "react-dom";
 
+const useNetwork = onchange => {
+  const [status, setStatus] = useState(navigator.onLine);
+  const handleChange = () => {
+    if (typeof onchange === "function") {
+      onchange(navigator.onLine);
+    }
+    setStatus(navigator.onLine);
+  };
+  useEffect(() => {
+    window.addEventListener("online", handleChange);
+    window.addEventListener("offline", handleChange);
+    () => {
+      window.removeEventListener("online", handleChange);
+      window.removeEventListener("offline", handleChange);
+    };
+  }, []);
+
+  return status;
+};
+
 const App = () => {
+  const handleNetworkChange = online => {
+    console.log(online ? "We just went online" : "We are Offline");
+  };
+  const onLine = useNetwork(handleNetworkChange);
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>{onLine ? "Online" : "Offline"}</h1>
       <p>lorem ipsum llalal </p>
     </div>
   );
